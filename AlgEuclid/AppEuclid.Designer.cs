4a75ce346b75e80ebafa979a160aadd3d0806c40
Timofei Teterin <timofei.teterin@inbox.ru>
2022-03-24 11:51:13 +0300

    Добавьте файлы проекта.

---
+++ AlgEuclid/AppEuclid.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace AlgEuclid {
+    partial class euclidForm {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(euclidForm));
+            this.firstNum = new System.Windows.Forms.NumericUpDown();
+            this.secondNum = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pageGCD = new System.Windows.Forms.TabPage();
+            this.euclidTablRec = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            ((System.ComponentModel.ISupportInitialize)(this.firstNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondNum)).BeginInit();
+            this.pageGCD.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.euclidTablRec)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // firstNum
+            // 
+            this.firstNum.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstNum.Location = new System.Drawing.Point(12, 9);
+            this.firstNum.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.firstNum.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.firstNum.Name = "firstNum";
+            this.firstNum.Size = new System.Drawing.Size(176, 29);
+            this.firstNum.TabIndex = 0;
+            this.firstNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.firstNum.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            // 
+            // secondNum
+            // 
+            this.secondNum.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondNum.Location = new System.Drawing.Point(194, 9);
+            this.secondNum.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.secondNum.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.secondNum.Name = "secondNum";
+            this.secondNum.Size = new System.Drawing.Size(176, 29);
+            this.secondNum.TabIndex = 1;
+            this.secondNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(377, 10);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(91, 28);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pageGCD
+            // 
+            this.pageGCD.Controls.Add(this.euclidTablRec);
+            this.pageGCD.Location = new System.Drawing.Point(4, 22);
+            this.pageGCD.Name = "pageGCD";
+            this.pageGCD.Padding = new System.Windows.Forms.Padding(3);
+            this.pageGCD.Size = new System.Drawing.Size(466, 377);
+            this.pageGCD.TabIndex = 1;
+            this.pageGCD.Text = "GCD";
+            this.pageGCD.UseVisualStyleBackColor = true;
+            // 
+            // euclidTablRec
+            // 
+            this.euclidTablRec.AllowUserToAddRows = false;
+            this.euclidTablRec.AllowUserToDeleteRows = false;
+            this.euclidTablRec.AllowUserToResizeColumns = false;
+            this.euclidTablRec.AllowUserToResizeRows = false;
+            this.euclidTablRec.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.euclidTablRec.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.euclidTablRec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.euclidTablRec.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.euclidTablRec.DefaultCellStyle = dataGridViewCellStyle2;
+            this.euclidTablRec.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.euclidTablRec.Location = new System.Drawing.Point(3, 3);
+            this.euclidTablRec.MultiSelect = false;
+            this.euclidTablRec.Name = "euclidTablRec";
+            this.euclidTablRec.ReadOnly = true;
+            this.euclidTablRec.RowHeadersVisible = false;
+            this.euclidTablRec.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.euclidTablRec.Size = new System.Drawing.Size(460, 371);
+            this.euclidTablRec.TabIndex = 4;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "i";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn1.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Q i";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn2.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "U i";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn3.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "V i";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn4.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "R i";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            this.dataGridViewTextBoxColumn5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn5.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "U i+1";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            this.dataGridViewTextBoxColumn6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn6.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.HeaderText = "V i+1";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            this.dataGridViewTextBoxColumn7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn7.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.HeaderText = "R i+1";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            this.dataGridViewTextBoxColumn8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn8.Width = 50;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.pageGCD);
+            this.tabControl1.Location = new System.Drawing.Point(13, 45);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(474, 403);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // euclidForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 457);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.secondNum);
+            this.Controls.Add(this.firstNum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "euclidForm";
+            this.Text = "AlgEuclid";
+            ((System.ComponentModel.ISupportInitialize)(this.firstNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondNum)).EndInit();
+            this.pageGCD.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.euclidTablRec)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown firstNum;
+        private System.Windows.Forms.NumericUpDown secondNum;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TabPage pageGCD;
+        private System.Windows.Forms.DataGridView euclidTablRec;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ AlgEuclid/AppEuclid.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AlgEuclid {
+    public partial class euclidForm : Form {
+        public euclidForm() {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e) {
+            if (firstNum.Value == 0 || secondNum.Value == 0) {
+                MessageBox.Show("Agrs are zero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int first = Convert.ToInt32(firstNum.Value);
+            int second = Convert.ToInt32(secondNum.Value);
+
+            if (first > 0 && second < 0) {
+                int t = first;
+                first = second;
+                second = t;
+            }
+
+            //if (first < 0 && second < 0) {
+            //    first *= -1;
+            //    second *= -1;
+            //}
+
+            #region Euclid Second Method
+
+            euclidTablRec.Rows.Clear();
+
+            int cnterRow = euclidTablRec.RowCount++; ;
+
+            if (cnterRow == 0) {
+                euclidTablRec[0, cnterRow].Value = cnterRow;
+
+                euclidTablRec[1, cnterRow].Style.BackColor = Color.LightGray;
+
+                euclidTablRec[2, cnterRow].Value = 1; // u_i
+                euclidTablRec[3, cnterRow].Value = 0; // v_i
+                euclidTablRec[4, cnterRow].Value = first; // r_i
+
+                euclidTablRec[5, cnterRow].Value = 0; // u_i+1
+                euclidTablRec[6, cnterRow].Value = 1; // v_i+1
+                euclidTablRec[7, cnterRow].Value = second; //r_i+1
+            }
+
+            bool flag = false;
+            while (Convert.ToInt32(euclidTablRec[7, cnterRow].Value) != 0) {
+                cnterRow = euclidTablRec.RowCount++;
+                euclidTablRec[0, cnterRow].Value = cnterRow;
+
+                euclidTablRec[1, cnterRow].Value = Convert.ToInt32(euclidTablRec[4, cnterRow - 1].Value) / Convert.ToInt32(euclidTablRec[7, cnterRow - 1].Value);
+
+                if (first < 0 && second > 0) {
+                    if (!flag) 
+                        euclidTablRec[1, cnterRow].Value = Convert.ToInt32(euclidTablRec[1, cnterRow].Value) - 1;
+                }
+
+                euclidTablRec[2, cnterRow].Value = euclidTablRec[5, cnterRow - 1].Value;
+                euclidTablRec[3, cnterRow].Value = euclidTablRec[6, cnterRow - 1].Value;
+                euclidTablRec[4, cnterRow].Value = euclidTablRec[7, cnterRow - 1].Value;
+
+                euclidTablRec[7, cnterRow].Value = Convert.ToInt32(euclidTablRec[4, cnterRow - 1].Value) - Convert.ToInt32(euclidTablRec[1, cnterRow].Value) * Convert.ToInt32(euclidTablRec[4, cnterRow].Value);
+                if (Convert.ToInt32(euclidTablRec[7, cnterRow].Value) != 0) {
+                    euclidTablRec[5, cnterRow].Value = Convert.ToInt32(euclidTablRec[2, cnterRow - 1].Value) - Convert.ToInt32(euclidTablRec[1, cnterRow].Value) * Convert.ToInt32(euclidTablRec[2, cnterRow].Value);
+                    euclidTablRec[6, cnterRow].Value = Convert.ToInt32(euclidTablRec[3, cnterRow - 1].Value) - Convert.ToInt32(euclidTablRec[1, cnterRow].Value) * Convert.ToInt32(euclidTablRec[3, cnterRow].Value);
+                } else {
+                    euclidTablRec[5, cnterRow].Style.BackColor = Color.LightGray;
+                    euclidTablRec[6, cnterRow].Style.BackColor = Color.LightGray;
+                }
+
+                flag = true;
+            }
+
+            euclidTablRec[2, cnterRow].Style.BackColor = Color.LightPink;
+            euclidTablRec[3, cnterRow].Style.BackColor = Color.LightPink;
+            euclidTablRec[4, cnterRow].Style.BackColor = Color.Yellow;
+            euclidTablRec[7, cnterRow].Style.BackColor = Color.LightGreen;
+
+            #endregion
+
+        }
+    }
+}
